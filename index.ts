b196190f5dac533e1b4d5ddbc160925de12df754
Daten0 <febrimicrone@gmail.com>
2024-11-24 14:25:46 +0800

    Merubah dari Bun.env menjadi process.env

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import express, { text } from "express";
 import multer from "multer";
 import nunjucks from "nunjucks";
 import open from "open";
+import Bun from 'bun';
+
 
 // Firebase
 import { initializeApp } from "@firebase/app";
@@ -17,21 +19,21 @@ import {
 } from "@firebase/auth";
 
 if (
-    !Bun.env["FIREBASE_API_KEY"] ||
-    !Bun.env["FIREBASE_AUTH_DOMAIN"] ||
-    !Bun.env["FIREBASE_PROJECT_ID"] ||
-    !Bun.env["FIREBASE_STORAGE_BUCKET"] ||
-    !Bun.env["FIREBASE_MESSAGING_SENDER_ID"] ||
-    !Bun.env["FIREBASE_APP_ID"]
+    !process.env["FIREBASE_API_KEY"] ||
+    !process.env["FIREBASE_AUTH_DOMAIN"] ||
+    !process.env["FIREBASE_PROJECT_ID"] ||
+    !process.env["FIREBASE_STORAGE_BUCKET"] ||
+    !process.env["FIREBASE_MESSAGING_SENDER_ID"] ||
+    !process.env["FIREBASE_APP_ID"]
 ) throw new Error("Autentikasi Firebase tidak lengkap. Mohon periksa file .env");
 
 const firebaseApp = initializeApp({
-    apiKey: Bun.env["FIREBASE_API_KEY"],
-    authDomain: Bun.env["FIREBASE_AUTH_DOMAIN"],
-    projectId: Bun.env["FIREBASE_PROJECT_ID"],
-    storageBucket: Bun.env["FIREBASE_STORAGE_BUCKET"],
-    messagingSenderId: Bun.env["FIREBASE_MESSAGING_SENDER_ID"],
-    appId: Bun.env["FIREBASE_APP_ID"]
+    apiKey: process.env["FIREBASE_API_KEY"],
+    authDomain: process.env["FIREBASE_AUTH_DOMAIN"],
+    projectId: process.env["FIREBASE_PROJECT_ID"],
+    storageBucket: process.env["FIREBASE_STORAGE_BUCKET"],
+    messagingSenderId: process.env["FIREBASE_MESSAGING_SENDER_ID"],
+    appId: process.env["FIREBASE_APP_ID"]
 });
 const firebaseAuth = getAuth(firebaseApp);
 const firestore = getFirestore(firebaseApp);
